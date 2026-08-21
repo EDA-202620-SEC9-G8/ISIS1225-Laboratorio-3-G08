f800e74a67b63c289baa271fa7e53c325b6970e6
LeonardoGit <gomezcifuentesleonardo@hmail.com>
2026-08-21 18:17:59 -0500

    primeros cambios en las funciones 2.2.1 array_list y single_linked_list, se agrego la funcion new_list() para crear una nueva lista vacia

---
+++ DataStructures/List/array_list.py
@@ -0,0 +1,6 @@
+def new_list():
+    newlist = {
+        "elements": [],
+        "size": 0
+    }
+    return newlist
---
+++ DataStructures/List/single_linked_list.py
@@ -0,0 +1,7 @@
+def new_list():
+    newlist = {
+        "first":None,
+        "last":None,
+        "size":0
+    }
+    return newlist
