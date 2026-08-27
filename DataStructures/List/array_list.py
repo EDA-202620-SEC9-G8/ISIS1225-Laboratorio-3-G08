c56ebea58275780e8c8f9a3d95f313a0026fccc4
dilandaza <d.dazab@uniandes.edu.co>
2026-08-26 23:20:42 -0500

    Funciones, get element y is present, se agregaron a la clase ArrayList.

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -4,3 +4,19 @@ def new_list():
         "size": 0
     }
     return newlist
+def get_element(my_list, index):
+    return my_list["elements"][index]
+
+def is_present(my_list, element, cmp_function):
+    size = my_list["size"]
+    if size > 0:
+        keyexist = False
+        for keypos in range(0, size):
+            info = my_list["elements"][keypos]
+            if cmp_function(element, info) == 0:
+                keyexist = True
+                break
+        if keyexist:
+            return keypos
+    return -1
+    
